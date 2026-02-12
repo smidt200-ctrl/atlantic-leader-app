c25c36b59b24700094af2c4b66fb55eb44077c59
smidt200-ctrl <smidt200@gmail.com>
2026-02-12 10:38:06 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,19 @@
-self.addEventListener('install', function(event) {
+const CACHE_NAME = "trawler-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('trawler-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
